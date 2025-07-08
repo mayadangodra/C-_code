e01f767352a248d328b6a9275e2418c4afccb0ec
mayadangodra <Dangodramaya910@gmail.com>
2025-07-08 18:19:54 +0530

    Exercise4.cpp

---
+++ Exercise4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <limits>
+using namespace std;
+
+int main() {
+   cout <<"Integer Min:" << numeric_limits<int>::min() << endl;
+   cout <<"Integer Max:" << numeric_limits<int>::max() << endl;
+   return 0;
+}
