73294732be26acb37d3bb78c49029faa8bc5ccb8
mayadangodra <Dangodramaya910@gmail.com>
2025-07-08 18:24:41 +0530

    Exercise7.cpp

---
+++ Exercise7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Enter a number:";
+    cin >> num;
+
+    if (num > 0)
+      cout <<"Positive number";
+    else if (num < 0)
+      cout <<"Negative number";
+    else
+      cout <<"Zero";
+    return 0;
+}
