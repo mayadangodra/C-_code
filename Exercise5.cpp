8603933fd481011d3c6c09c937e3b9a766b032ae
mayadangodra <Dangodramaya910@gmail.com>
2025-07-08 18:21:51 +0530

    Exercise5.cpp

---
+++ Exercise5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num = 57;
+  cout <<"The number is:" << num << endl;
+  cout <<"After post increment by 1 the number is:" << num++ << endl;
+  cout <<"After increment, the number is:" << num << endl;
+  cout <<"After pre increment by 1 the number is:" << ++num << endl;
+  cout <<"After increasing by 1 the number is:" << num << endl;
+  cout <<"After post decrement by 1 the number is:" << num-- << endl;
+  cout <<"After decrement, the number is:" << num << endl;
+  cout <<"After pre decrement by 1 the number is:" << --num << endl;
+  cout <<"After decreasing by 1 the number is:" << num << endl;
+  return 0;
+}
