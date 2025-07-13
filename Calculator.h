fec129ce2909c8376ef243a87a615d6f1563504d
mayadangodra <Dangodramaya910@gmail.com>
2025-07-13 17:22:21 +0530

    Calculator.h

--- Calculator.h
+++ Calculator.h
@@ -8,9 +8,9 @@ int subtract(int a, int b){
    return a-b;
 }
 
-int subtract(int a, int b){
+int multiply(int a, int b){
    return a*b;
 }
-int subtract(int a, int b){
+int divide(int a, int b){
      return a/b;
 }
