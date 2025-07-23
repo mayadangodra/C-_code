053b1222ecea0d92db35f675998bc3e5aad62cc3
mayadangodra <Dangodramaya910@gmail.com>
2025-07-23 12:46:23 +0530

    Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -15,19 +15,19 @@ cin>>a>>b;
 
 switch (op){
   case '+':
-    cout<<"sum"<<add(a+b)<<endl;
+    cout<<"sum:"<<add(a+b)<<endl;
     break;
 
   case '-':
-     cout<<"substract"<<subtract(a-b)<<endl
+     cout<<"substract:"<<subtract(a-b)<<endl
      break;
 
   case '*':
-    cout<<"multiply"<<multiplye(a*b)<<endl
+    cout<<"multiply:"<<multiplye(a*b)<<endl
     break;
 
   case '/'
-    cout<<"divide"<<divide(a/b)<<endl
+    cout<<"divide:"<<divide(a/b)<<endl
     break;
 
   default:
